cc84d2feb817deecf15e12f4154d900bc3062fff
AntoninaBT <gorbaa533@gmail.com>
2024-10-12 17:52:41 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,10 @@
+# Teplova // Create the first variable (my name), output, create the second variable (my age), out it. Then rewrite the current variable + new
+# Create a variable Is_student and output. Use print and "" for strings, don't forget to use _ when write two more words in the variable. 
+name = "Antonina"
+print(name)
+age = 32
+print(age)
+new_age = age + 31
+print(new_age)
+Is_student = "True"
+print(Is_student)
